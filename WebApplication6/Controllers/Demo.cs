6381b555f11e9e2a3a46a92603e596b0e5459266
Gauri <gaurijindal.hrd@gmail.com>
2024-05-07 00:12:52 +0530

    Add project files.

---
+++ WebApplication6/Controllers/Demo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class Demo : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Policy()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
